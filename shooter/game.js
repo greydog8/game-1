892555101059d918293710d53b686b7ccc2c2139
Greyson Siebenhar <siebgrey@gmail.com>
2015-07-25 11:53:14 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -1,21 +1,33 @@
 'use strict';
+var ship;
+var cursors;
 
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload,create:create,update:update,render:render});
 
 
-
 function preload(){
   game.stage.backgroundColor = '#bbbbbb';
-  game.load.image('ship', 'ship.png');
+  game.load.image('ship', 'ship');
 }
 
 function create(){
   ship = game.add.sprite(400,550,'ship');
+  game.physics.enable(ship, Phaser.Physics.ARCADE);
+  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
+  ship.body.velocity.x = 0;
+  ship.body.velocity.y = 0;
+  if (cursors.down.isDown) {
+    ship.body.velocity.y = -300;
+  }
+else if (cursors.down.isDown){
+  ship.body.velocity.y = 300;
+  }
 }
+
 
 function render(){
 }
