d1003f5897a97332466cd7080ad858aa943f2aaf
Greyson Siebenhar <siebgrey@gmail.com>
2015-08-01 11:57:36 -0400

    save

--- shooter/game.js
+++ shooter/game.js
@@ -1,18 +1,24 @@
 'use strict';
 var ship;
 var cursors;
-
+var background;
+var missle;
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload,create:create,update:update,render:render});
 
 
 function preload(){
   game.stage.backgroundColor = '#bbbbbb';
-  game.load.image('ship', 'ship');
+  game.load.image('background','background.gif');
+  game.load.image('missle','missle.gif');
+  game.load.image('ship', 'ship.gif');
 }
 
 function create(){
-  ship = game.add.sprite(400,550,'ship');
+  background = game.add.tileSprite(0,0,800,600,'background');
+  background.autoScroll(-300,0);
+  ship = game.add.sprite(20,300,'ship');
+  missle = game.add.sprite(20,300,'missle');
   game.physics.enable(ship, Phaser.Physics.ARCADE);
   cursors = game.input.keyboard.createCursorKeys();
 }
@@ -20,7 +26,9 @@ function create(){
 function update(){
   ship.body.velocity.x = 0;
   ship.body.velocity.y = 0;
-  if (cursors.down.isDown) {
+  missle.x = ship.x + 65
+  missle.y = ship.y + 35
+  if (cursors.up.isDown) {
     ship.body.velocity.y = -300;
   }
 else if (cursors.down.isDown){
